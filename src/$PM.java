25a1979db1bf842b95f5cdee019f11abefd9bdc6
Paul Miskew <paul.miskew@gmail.com>
2017-03-31 17:00:58 -0400

    first commit

---
+++ src/$PM.java
@@ -0,0 +1,18 @@
+
+public class $PM {
+
+
+	public static void println(String word) {
+		
+		System.out.println(word);
+		
+	} //end println
+	
+	public static void print(String word) {
+		
+		System.out.print(word);
+	
+	} //end print
+
+
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// THIS IS WHERE THE PROGRAM STARTS
+		$PM.print("Hello World");
+		$PM.print("Hello World Again");
+	
+		
+		
+	} //end main
+
+	
+	
+} //end HelloWorld
