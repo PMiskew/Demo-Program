bddb7f29e63bf826a0e5b42a7316138b396db626
Paul Miskew <paul.miskew@gmail.com>
2017-03-31 17:03:50 -0400

    second commit

---
+++ src/ToolTester.java
@@ -0,0 +1,15 @@
+/**
+ * This file is designed to simply test our tools.  My tools class is called $PM
+ * 
+ * @author PMiskew
+ *
+ */
+public class ToolTester {
+
+	public static void main(String[] args) {
+	
+		$PM.println("Tool Tester");
+		
+	}
+
+}
